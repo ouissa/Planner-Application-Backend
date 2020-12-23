e34a636639c5cb2549bfe93336c3aff84e30a64f
Glitch (planner-back-end) <none>
2020-12-23 00:05:44 +0000

    ⏱👼 Checkpoint ./config/db.js:23282999/1718

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ const mongoUsername = process.env.username;
 const mongoPassword = process.env.password;
 
 const MONGOURI = `mongodb+srv://${mongoUsername}:${mongoPassword}@planner.jqdzy.mongodb.net/planner_database?retryWrites=true&w=majority`;
+const allCourses = [];
 
 const InitiateMongoServer = async () => {
   const client = new MongoClient(MONGOURI);
@@ -13,7 +14,16 @@ const InitiateMongoServer = async () => {
     // Connect to the MongoDB cluster
     await client.connect();
     console.log("Successfully Connested to The Planner Database");
-    return client
+        const stg = await client
+      .db("planner_database")
+      .collection("courses_collection")
+      .find({})
+      .toArray(function(err, result) {
+        if (err) throw err;
+        list.push(result);
+        console.log(result);
+    });
+
 
   } catch (e) {
     console.error(e);
@@ -22,3 +32,4 @@ const InitiateMongoServer = async () => {
 
 
 module.exports = InitiateMongoServer;
+module.exports = allCourses;
