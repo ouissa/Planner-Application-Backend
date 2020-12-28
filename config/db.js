dc916e72a1ea65f5ebdd8591d263d3294096b4b9
Glitch (planner-back-end) <none>
2020-12-28 22:25:19 +0000

    🚖🌌 Checkpoint ./config/db.js:23282999/3433 ./index.js:23282999/109 ./models/DegreePlan:23282999/579 ./models/DegreePlan.js:23282999/968

--- config/db.js
+++ config/db.js
@@ -1,30 +1,19 @@
 const mongoose = require("mongoose");
-const { MongoClient } = require("mongodb");
 
 const mongoUsername = process.env.username;
 const mongoPassword = process.env.password;
 
 const MONGOURI = `mongodb+srv://${mongoUsername}:${mongoPassword}@planner.jqdzy.mongodb.net/planner_database?retryWrites=true&w=majority`;
-const allCourses = [];
 
 const InitiateMongoServer = async () => {
   try {
-    const client = new MongoClient(MONGOURI);
-    // Connect to the MongoDB cluster
-    await client.connect();
-    console.log("Successfully Connested to The Planner Database");
-    const stg = await client
-      .db("planner_database")
-      .collection("courses_collection")
-      .find({})
-      .toArray(function(err, result) {
-        if (err) throw err;
-        allCourses.push(result);
-
-      });
-    return allCourses
+    await mongoose.connect(MONGOURI, {
+      useNewUrlParser: true
+    });
+    console.log("Connected to Planner DB !!");
   } catch (e) {
-    console.error(e);
+    console.log(e);
+    throw e;
   }
 };
 
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const semester = require("./routes/semester"); //new addition
-const course = require("./routes/course"); //new addition
 const InitiateMongoServer = require("./config/db");
 const path = require("path");
 var cors = require("cors");
 
 // Initiate Mongo Server
+InitiateMongoServer();
 
 
 const app = express();
@@ -19,7 +19,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/semesters", semester);
-app.use("/course", course);
 
 app.listen(PORT, (req, res) => {
   console.log("Server Started at PORT ${PORT}");
