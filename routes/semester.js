3063b25e7f1a58c4c52f52edcd0d237b10a16a81
Glitch (planner-back-end) <none>
2020-12-29 03:01:33 +0000

    🍢🌆 Checkpoint ./routes/semester.js:23282999/1389 ./index.js:23282999/22

--- routes/semester.js
+++ routes/semester.js
@@ -109,4 +109,16 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.get("/", async (req, res) => {
+  try {
+
+    res.json({"plan": true});
+    console.log(
+      "Semester Endpoint here (GET OPERATION): Communication with the front-end done"
+    );
+  } catch (e) {
+    res.send({ message: "Error in Fetching semester information" });
+  }
+});
+
 module.exports = router;
