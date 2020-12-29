79eab32a65baa0b30fb578fc0abf2ce28cdeb842
Glitch (planner-back-end) <none>
2020-12-29 00:21:36 +0000

    🍬📍 Checkpoint ./routes/semester.js:23282999/1142

--- routes/semester.js
+++ routes/semester.js
@@ -98,69 +98,7 @@ router.post("/", async (req, res) => {
 
 router.get("/", async (req, res) => {
   try {
-    const plan = await Plan.find({});
-    var list = [
-      {
-        id: "semester-0",
-        title: "Fall 2018",
-        courses: []
-      },
-      {
-        id: "semester-1",
-        title: "Spring 2019",
-        courses: []
-      },
-      {
-        id: "semester-2",
-        title: "Summer 2019",
-        courses: []
-      },
-      {
-        id: "semester-3",
-        title: "Fall 2019",
-        courses: []
-      },
-      {
-        id: "semester-4",
-        title: "Spring 2020",
-        courses: []
-      },
-      {
-        id: "semester-5",
-        title: "Summer 2020",
-        courses: []
-      },
-      {
-        id: "semester-6",
-        title: "Fall 2020",
-        courses: []
-      },
-      {
-        id: "semester-7",
-        title: "Spring 2021",
-        courses: []
-      },
-      {
-        id: "semester-8",
-        title: "Summer 2021",
-        courses: []
-      },
-      {
-        id: "semester-9",
-        title: "Fall 2021",
-        courses: []
-      },
-      {
-        id: "semester-10",
-        title: "Spring 2022",
-        courses: []
-      },
-      {
-        id: "semester-11",
-        title: "Summer 2022",
-        courses: []
-      }
-    ];
+    const plan = await Plan.find({"student"});
     res.json(plan);
     console.log(
       "Semester Endpoint here (GET OPERATION): Communication with the front-end done"
